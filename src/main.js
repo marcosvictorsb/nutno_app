0665d1ad5b5e0d9b86577d0cb54b681c1730317a
marcos <marcosvictorsb@gmail.com>
2026-03-06 17:37:42 -0300

    fix: update asset import paths in main.js for consistency

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 
-import '@/assets/styles.scss';
-import '@/assets/tailwind.css';
+import './assets/styles.scss';
+import './assets/tailwind.css';
 
 const app = createApp(App);
 
